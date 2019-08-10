48f813b0f4c83fd619437da29bca24eedc8cc9d3
sarita <sarita.chetak@gmail.com>
2019-08-11 00:38:48 +0100

    hjjk

---
+++ src/academy/learnProgramming/MyCommentsTest.java
@@ -0,0 +1,61 @@
+
+package academy.learnProgramming;
+
+//this is my class definition asdfsdfsdffsafdsdfsdffsda
+//sflksajflfjsadlfjsdflkjfkldsjlfsksalkfdjsadfj
+///387479woisfdjksdlfkslf
+
+/*
+ * sdkfjsaldkfjsdf
+ * laskjdflksjadf
+ * kjasldkfjslafd
+ * laksjdflksjadf
+ */
+
+public class MyCommentsTest {
+    /**
+     *
+     * @param args  command line arguments
+     */
+    public static void main(String[] args) {
+        // single line comment;
+        //
+        //
+        System.out.println("Hello comments"); //ctrl+slash ctrl+shift+slash
+
+        /*
+        System.out.println("Comment 1");
+        System.out.println("Comment 2");
+        System.out.println("Comment 3");
+        System.out.println("Comment 4");
+        System.out.println("Comment 5");
+        System.out.println("Comment 6");
+        System.out.println("Comment 7");
+        */
+        System.out.println("sum of 3 & 4 is " + sum(3,4));
+    }
+
+    public int anotherSum(int a, int b){
+        return a+b;
+    }
+
+    /**
+     *
+     * @param a    this is first parameter
+     * @param b    this is second parameter
+     * @return     this is sum of a & b
+     */
+    public static int sum(int a, int b){
+        return (a+b);
+    }
+
+    /**
+     *
+     * @param x
+     * @param y
+     */
+
+    public void newMethod(int x, int y){
+
+    }
+}
